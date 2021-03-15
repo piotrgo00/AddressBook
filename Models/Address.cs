cda7e08cddeed9d6324db4a8a3c303e9b121147c
piotrgo00 <65816799+piotrgo00@users.noreply.github.com>
2021-03-15 11:39:59 +0100

    Dodaj pliki projektów.

---
+++ Models/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Models
+{
+    public class Address
+    {
+        [Display(Name = "Twoja ulubiona ulica"), Required(ErrorMessage = "Pole obowiazkowe"), StringLength(30, MinimumLength = 5)]
+        public string Street { get; set; }
+
+        [Required]
+        public int Number { get; set; }
+
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+    }
+}
